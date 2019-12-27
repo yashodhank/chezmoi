8d9fae9feb898fdd6795b6a5ead88c86eb183652
Tom Payne <twpayne@gmail.com>
2019-12-27 15:46:00 +0000

    Switch to github.com/rakyll/statik for assets

--- cmd/helps.go
+++ cmd/helps.go
@@ -1,3 +1,3 @@
 package cmd
 
-//go:generate go run github.com/twpayne/chezmoi/internal/extract-helps -i ../docs/REFERENCE.md -o helps.gen.go
+//go:generate go run github.com/twpayne/chezmoi/internal/extract-helps -i ../public/docs/REFERENCE.md -o helps.gen.go
